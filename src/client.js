3a07b387e63000c188ed13e6b6d049fddc1db321
redpmorg <leonardlepadatu@yahoo.com>
2017-07-13 23:56:59 +0300

    salvare

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as StyledLibrary from './client1-4.js';
-import Helpers from './helpers.js'
+// import Helpers from './helpers.js'
 
 var $ = require('jquery');
 
@@ -19,40 +19,48 @@ class App extends React.Component {
       msg: '',
       results: []
     }
-    this._onSubmit = this._onSubmit.bind(this);
-    this._onChange = this._onChange.bind(this);
+    // this._onSubmit = this._onSubmit.bind(this);
+    // this._onChange = this._onChange.bind(this);
   }
 
   componentDidMount() {
     console.log('App component is mounted', this.props)
   }
 
-  _onSubmit(e) {
+  _onSubmit = (e) => {
     e.preventDefault();
-    var value = Helpers.nodeFind(this).value;
-    this.postToServer.apply(this, {
+    console.log(this.refs);
+    var name = ReactDOM.findDOMNode(this.refs.name).value;
+    this.postToServer(e.type, {
       url: 'myPage',
-      data: [name : value]
-    })
+      data: [name : name]
+    });
   }
 
-  _onChange(e) {
+  _onChange = (e) => {
     let value = e.target.value;
-    this.setState(state => ({
-      results: state.results.concat([value])
-    }));
+    this.postToServer(e.type, {url: 'get', data: value});
+    // this.setState(state => ({
+    //   results: state.results.concat([value])
+    // }));
   }
 
-  postToServer(data) {
+  postToServer(scope, data) {
     let self = this;
     $.ajax({
+      method: 'POST',
+      contentType: "application/json",
       url: data.url,
       dataType: 'json',
       cache: false,
       data: data.data,
-      method: 'POST',
+      dataType: "JSON",
       success: function(response) {
-        self.setState({msg: response.key});
+        if (scope == "change") {
+          self.setState({results: response});
+        } else if (scope == "submit") {
+          self.setState({msg: response});
+        }
       },
       error: function(xhr, status, err) {
         self.setState({
@@ -67,9 +75,13 @@ class App extends React.Component {
       <div>
         <form className="form-inline" onSubmit={this._onSubmit}>
           <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input className="form-control" ref='email' type='text' placeholder='Enter email' name='email' autoComplete="off"/>
+          </div>
+          <div className="form-group">
             <label htmlFor="name">Name:</label>
-            <input className="form-control " ref='name' type='text' placeholder='Enter name' name='name' onChange={this._onChange}/>
-            <AutoSuggest results= { this.state.results } />
+              <input className="form-control" ref='name' type='text' placeholder='Enter name' name='name' onChange={this._onChange} autoComplete="off"/>
+              <AutoSuggest results={this.state.results}/>
           </div>
           <Submit name="Trimite" className="btn btn-primary" type="submit"/>
         </form>
@@ -78,21 +90,24 @@ class App extends React.Component {
     )
   }
 }
-
 class AutoSuggest extends React.Component {
   render() {
     return (
-      <ul>
-        { console.log(this.props) }
-        { this.props.results.map((item, index) => (<li key={index}> {item} </li>))}
+      <ul className="ui-autocomplete">
+        {this.props.results.map((item, index) => (
+          <li key={index}>
+            {item}
+          </li>
+        ))}
       </ul>
-    )
-  }
+  )}
 }
 
 ReactDOM.render(
   <div>
-  <StyledLibrary.Hello click="Click Me!" clicked="That's crazy! Click again!"/>
-  <StyledLibrary.Clock/>
-  <App myProps="myProps"/>
-</div>, document.getElementById('one'));
+    <StyledLibrary.Hello click="Click Me!" clicked="That's crazy! Click again!"/>
+    <StyledLibrary.Clock/>
+    <App myProps="myProps"/>
+  </div>,
+  document.getElementById('one')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, 'src', '/client.js'),
+  entry: path.join(__dirname, 'src', '/app.js'),
   output: {
     path: path.resolve(__dirname, './public'),
     filename: 'bundle.js'
